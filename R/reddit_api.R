913df5c346c082ac972b5695954e74702bbb9b7a
Patrick Mcgonagle <mcgon046@umn.edu>
2026-03-30 02:04:11 -0500

    save

--- R/reddit_api.R
+++ R/reddit_api.R
@@ -3,6 +3,21 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 library(httr2) # Apparently this is the updated httr library 
 library(jsonlite)
+library(RedditExtractoR)
 
 # Data Import and Cleaning
 
+## Pulls data from reddit based on assignment specifications (posts from the last month)
+rstats_urls <- find_thread_urls(
+  keywords = NA, 
+  sort_by = "top",
+  subreddit = "rstats", 
+  period = "month"
+) 
+
+## Get upvotes 
+rstats_upvotes <- get_thread_content(rstats_urls$url)
+
+# Visualization 
+
+# Analysis 
