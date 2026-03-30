ffec7ea2b11e782026b78e299c5700388717a8df
Patrick Mcgonagle <mcgon046@umn.edu>
2026-03-30 12:14:08 -0500

    Finished? running from source to make sure

--- R/reddit_api.R
+++ R/reddit_api.R
@@ -48,3 +48,23 @@ df_val <- rstats_cor$parameter # Degrees of
 cor_val <- rstats_cor$estimate
 p_val <- rstats_cor$p.value
 
+# Publication 
+# The correlation between upvotes and comments was r(71) = .34, p = .00. This test was statistically significant.
+signif <- ifelse(p_val < 0.05, "was", "was not") # Significance logic for subsequent console output 
+
+df_fmt <- round(df_val) # Rounded so no 0s 
+
+cor_temp <- sprintf("%.2f", cor_val) # temporary variable to only have lst two digits of correlation 
+p_temp   <- sprintf("%.2f", p_val) # same as above but for p value
+
+cor_fmt <- sub("^0", "", cor_temp) # Gets rid of leading 0 in correlation 
+p_fmt <- sub("^0", "", p_temp) # Same as above for p value 
+
+# Final string to be produced in console 
+final_text <- paste0(
+  "The correlation between upvotes and comments was r(", 
+  df_fmt, ") = ", cor_fmt, ", p = ", p_fmt, 
+  ". This test ", signif, " statistically significant."
+)
+
+cat(final_text)
