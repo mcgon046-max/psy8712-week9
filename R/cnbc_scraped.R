f621bdab4edc0d9dd45eb814b89047ac0ef61b1f
Patrick Mcgonagle <mcgon046@umn.edu>
2026-03-30 03:08:21 -0500

    Working on web scraping data

--- R/cnbc_scraped.R
+++ R/cnbc_scraped.R
@@ -4,3 +4,35 @@ library(tidyverse)
 library(rvest)
 
 # Data Import and Cleaning 
+## Subpages of cnbc into a list
+cnbc_pages <- list(
+  Business = "https://www.cnbc.com/business/",
+  Investing = "https://www.cnbc.com/investing/",
+  Tech = "https://www.cnbc.com/technology/",
+  Politics = "https://www.cnbc.com/politics/"
+)
+
+## Empty list to hold scraped content for assignment 
+cnbc_data_list <- list()
+
+## For loop to get all the data
+ for(section in names(cnbc_pages) {
+   
+   # Get the current URL so that this loop reads the data every time 
+   curr_url <- cnbc_pages[[section]]
+   
+   # Pull the data 
+   page_html <- read_html(curr_url)
+   
+   # Get headlines 
+   headlines_txt <- page_html |>
+     html_elements(".Card-title") |> #CSS selector for headlines on website 
+     html_text()
+   
+   # Data fram
+     
+   
+     
+     
+   
+ }
