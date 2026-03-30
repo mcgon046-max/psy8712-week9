393e67a6c53db383c40ed0fd6d65d8800bc6ebd5
Patrick Mcgonagle <mcgon046@umn.edu>
2026-03-30 01:43:03 -0500

    save

--- R/reddit_api.R
+++ R/reddit_api.R
@@ -1,3 +1,8 @@
 # Script Settings and Resources 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
+library(httr2) # Apparently this is the updated httr library 
+library(jsonlite)
+
+# Data Import and Cleaning
+
